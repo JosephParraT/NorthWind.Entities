165c9de815863fa17ef36702fec89e82e73fef3b
JosephParraT <Josephpt2004@outlook.com>
2024-12-08 16:55:18 -0500

    Ajustes a program.cs ajustando salida

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -1,11 +1,21 @@
-﻿using NorthWind.ConsoleApp.Services;
+﻿using Microsoft.Extensions.Hosting;
+using NorthWind.ConsoleApp.Services;
 using NorthWind.Entities.Interfaces;
 using NorthWind.Writers;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
 
 IUserActionWriter Writer = new ConsoleWriter();
+HostApplicationBuilder Builder = Host.CreateApplicationBuilder();
+Builder.Services.AddSingleton<IUserActionWriter, ConsoleWriter>();
+Builder.Services.AddSingleton<AppLogger>();
+Builder.Services.AddSingleton<ProductService>();
+using IHost AppHost = Builder.Build();
 
 AppLogger Logger = new AppLogger(Writer);
-Logger.WriteLog("Aplication Started.");
+AppLogger Logger = AppHost.Services.GetRequiredService<AppLogger>();
+Logger.WriteLog("Aplicacion started.");
 
 ProductService Service = new ProductService(Writer);
-Service.add("Demo","Azucar Refinada");
+ProductService Service = AppHost.Services.GetRequiredService<ProductService>();
+Service.Add("Demo", "Azucar refinada");
