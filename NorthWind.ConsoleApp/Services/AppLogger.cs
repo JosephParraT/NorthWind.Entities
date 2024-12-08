026511a8484e86b197517f07cc792678717502b6
JosephParraT <Josephpt2004@outlook.com>
2024-12-08 14:28:11 -0500

    Cambios

---
+++ NorthWind.ConsoleApp/Services/AppLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.ConsoleApp.Services
+{
+    internal class AppLogger(IUserActionWriter writer)
+    {
+        public void WriteLog(string message) 
+            {
+            UserAction Log = new  UserAction("System", message);
+            writer.Write(Log);
+            }
+    }
+}
